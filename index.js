ab879ddd960208041d0fd43c6e39e5f7259184cd
Andrew <andrewchoi1@hotmail.com>
2019-02-03 17:58:43 -0500

    created new user accounts

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:auth/auth');
+mongoose.connect('mongodb://localhost:auth/auth', {
+    useNewUrlParser: true
+});
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*'}));
